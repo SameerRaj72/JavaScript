0021d3d88df4f6b0cadc983c20162baaebe4145f
SameerRaj72 <sameerazz727464@gmail.com>
2024-07-10 10:35:31 +0000

    done

---
+++ Objects3.js
@@ -0,0 +1,22 @@
+// const course={
+//     coursename:"js in hindi",
+//     price:"999",
+//     courseInstructor:"Hitesh"
+// }
+
+//course.courseInstructor
+//const {courseInstructor:instructor}=course
+//console.log(courseInstructor);
+//console.log(instructor);
+
+//Destructure in react
+// const navbar=({company})=>{
+
+// }
+// navbar(company="hitesh")
+
+// {
+//    "name":"samir",
+//     "coursename":"js in hindi",
+
+// }
